dc65553b6c1e1f0a0c51e10cf9a5a98251991243
Kshamenk Nehuén Gil <7087206+kshamenkgil@users.noreply.github.com>
2022-03-25 22:10:03 -0300

    BlockWallet ledger bridge

--- bundle.js
+++ bundle.js
@@ -370,7 +370,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 (async function () {
     var bridge = new _ledgerBridge2.default();
 })();
-console.log('MetaMask < = > Ledger Bridge initialized from ' + window.location + '!');
+console.log('BlockWallet < = > Ledger Bridge initialized from ' + window.location + '!');
 
 },{"./ledger-bridge":1}],3:[function(require,module,exports){
 "use strict";
--- main.js
+++ main.js
@@ -4,4 +4,4 @@ import LedgerBridge from './ledger-bridge'
 (async () => {
     const bridge = new LedgerBridge()
 })()
-console.log(`MetaMask < = > Ledger Bridge initialized from ${window.location}!`)
+console.log(`BlockWallet < = > Ledger Bridge initialized from ${window.location}!`)
